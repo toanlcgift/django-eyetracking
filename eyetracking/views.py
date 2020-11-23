94543a3d56b592979a1437aa78e0981fdf0ac2c6
toannd <toannd@DESKTOP-JHFF80P>
2020-11-23 17:46:47 +0700

    raw

--- eyetracking/views.py
+++ eyetracking/views.py
@@ -43,12 +43,6 @@ def upload_image(request):
               elif gaze.is_center():
                     text = "Looking center"
 
-              cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
-
-              left_pupil = gaze.pupil_left_coords()
-              right_pupil = gaze.pupil_right_coords()
-              cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-              cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
               channel_layer = get_channel_layer()
               async_to_sync(channel_layer.group_send)("room-1", 
                 {
